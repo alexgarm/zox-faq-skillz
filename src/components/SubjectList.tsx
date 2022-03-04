c1962dbf109c573ba7273958e9726b36c7b5d45f
alexgar <you@example.com>
2022-03-04 12:05:32 -0600

    item

--- src/components/SubjectList.tsx
+++ src/components/SubjectList.tsx
@@ -1,13 +1,18 @@
 import React, {FC}  from "react";
 import { ISubject } from "../types/types";
+import Subject from "./Subject";
+import data from '../data/data'
 
 type Props = {
-    subjects: ISubject
+    data: any
 }
 
-const SubjectList =({subjects}: Props)=> {
+const SubjectList =({data}: Props)=> {
     return(
+        <div> {data.map(item => {item.title})}</div>
        
+     
+
     )
 
 }
